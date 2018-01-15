1bf14197dd9e85a56dec53d4631c51fb677f8b7c
Tatsuaki <machida@mi.t.u-tokyo.ac.jp>
2018-01-15 15:40:32 +0900

    add download script

---
+++ face-alignment/download_model.sh
@@ -0,0 +1,2 @@
+wget https://github.com/AKSHAYUBHAT/TensorFace/blob/master/openface/models/dlib/shape_predictor_68_face_landmarks.dat?raw=true -nc
+mv shape_predictor_68_face_landmarks.dat?raw=true shape_predictor_68_face_landmarks.dat
